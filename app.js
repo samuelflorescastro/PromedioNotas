0591798dac83c98ac2420457df3b27e5ab8489fa
Juan Perez <juan@email.com>
2026-07-20 21:40:24 -0400

    Se modifica el sistema para calcular cinco notas

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const nota1 = 95; 
 const nota2 = 85;
 const nota3 = 90;
+const nota4 = 100;
+const nota5 = 100;
 
-const promedio = (nota1 + nota2 + nota3) / 3;
+const promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
 
 console.log("Sistema de Promedios");
 console.log("-------------------");
 console.log("Nota 1:", nota1);
 console.log("Nota 2:", nota2);
 console.log("Nota 3:", nota3);
+console.log("Nota 4:", nota4);
+console.log("Nota 5:", nota5);
 console.log("Promedio:", promedio.toFixed(2));
 console.log("	");
 console.log("Fin del programa");
